ab48f74aecfacd62423aadc2bb233617bf0e72ff
nardaiv <92304461+nardaiv@users.noreply.github.com>
2023-03-27 16:53:33 +0700

    adding changeUnit and renderTemp Function

--- main/main.ino
+++ main/main.ino
@@ -20,6 +20,13 @@ unsigned long lastlcd = 0;
 unsigned long lastbtn = 0;
 int lastBtnValue;
 
+//define variable for symbol usage
+bool degreeUse = true;
+char symbol[20]; 
+
+//define temp variable
+float temp;
+
 //custom character to create degree symbol
 byte degree[] = {
   B01100,
@@ -35,12 +42,18 @@ byte degree[] = {
 
 void setup()
 {  
+  //setup terminal for debuging purposes
   Serial.begin(9600);
+  
+  //initialize lcd
   lcd.begin(16,2);
-  //lcd.init();
   lcd.backlight();
+  
+  //print booting message
   lcd.print("Temperature Sensor");
   pinMode(BUTTON_PIN, INPUT_PULLUP);
+  
+  //delay before start measure temperature
   delay(500);
 }
 
@@ -53,16 +66,24 @@ void loop()
   if(currentTime - lastbtn >= btnInt){
     int btnValue = digitalRead(BUTTON_PIN);
     lastbtn = currentTime;
+    
     if(btnValue==LOW && btnValue!=lastBtnValue ){
+      //increase counter value for every click
       counter++;
-      Serial.println(counter);
       
+      //return anything greater than 4 to 0
+      //so counter is always between 0 to 3
+      if(counter>=4){
+          counter =0;
+      }
+
+      //for checking counter
+      Serial.println(counter);
+        
   	}
+    
     lastBtnValue= btnValue;
     
-    if(counter>=2){
-      	counter =0;
-    }
   }
   
   
@@ -71,29 +92,79 @@ void loop()
     //update the condition var
     lastlcd = currentTime;
     
-    //rendering function
+    //clearing lcd
     lcd.clear();
-    float temp = analogRead(pinTemp);    //Read the analog pin
-    temp = (20*temp-2035)/41;  //convert output (mv) to readable celcius
-    char symbol[20] = "C"; 
     
-    //change to fahrenheit
-    if(counter ==1){
-    	temp *= 1.8;
-    	temp += 32;
-        strcpy(symbol,"F");
-    }
-    lcd.createChar(0,degree);
-    lcd.setCursor(13,0);
-    lcd.write(byte(0));
-    lcd.setCursor(0,0);
-    lcd.print("Temp: ");
-    lcd.print(temp);
-    lcd.setCursor(14,0);
-    lcd.print(symbol);
+    //taking temperature input
+    temp = analogRead(pinTemp);    //Read the analog pin
+    temp = (20*temp-2035)/41;  //convert output (mv) to readable celcius
+	
+    //changing unit based on click
+    changeUnit(counter);
+    
+    //rendering temperature
+	renderTemp(temp,degreeUse);
     
   }
   
   
 }
 
+
+float changeUnit(int type){
+  switch(type){
+    
+    //celcius
+    case 0:
+    	temp *= 1;
+    	degreeUse = true;
+    	strcpy(symbol,"C");
+    	break;
+    
+    //fahrenheit
+    case 1:
+    	temp *= 1.8;
+    	temp += 32;
+    	degreeUse = true;
+    	strcpy(symbol,"F");
+    	break;
+    
+    //reamur
+    case 2:
+    	temp *= 0.8;
+    	degreeUse = true;
+    	strcpy(symbol,"R");
+    	break;
+    
+    //kelvin
+    case 3:
+    	temp += 273;
+    	degreeUse = false;
+    	strcpy(symbol,"K");
+    	break;
+    
+    //unmatched input
+    default:
+    	Serial.println("invalid input (out of the switch options)");
+  
+  }
+  
+}
+
+void renderTemp(float temp, bool degreeStatus){
+  
+  //show degree if necessary 
+  if(degreeStatus == true){
+    lcd.createChar(0,degree);
+    lcd.setCursor(13,0);
+    lcd.write(byte(0));
+  }
+  
+  //show current temperature
+  lcd.setCursor(0,0);
+  lcd.print("Temp: ");
+  lcd.print(temp);
+  lcd.setCursor(14,0);
+  lcd.print(symbol);
+  
+}
